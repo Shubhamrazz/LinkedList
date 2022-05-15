1b38ea5005a1b9c6e7fe6afbf9e77d322630c1fe
Shubhamrazz <sr99845@gmail.com>
2022-05-15 19:42:37 +0530

    [Shubham]Create Simple Linked List of 56,30 and 70

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("----Welcome to Linked List Program----");
+            //Calling Class LinkedList
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+
+            Console.ReadLine();
+        }
+    }
+}
